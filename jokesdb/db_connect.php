1b9ec9ef914390eb850a71f6bdc3e7020f4580d2
maksmac <33382740+maksmac@users.noreply.github.com>
2019-12-03 11:36:27 -0700

    Update db_connect.php

--- jokesdb/db_connect.php
+++ jokesdb/db_connect.php
@@ -2,7 +2,7 @@
     $host = "hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
     $username = "e48oxaowx6xev76t";
     $password = "amgpi1p16ldin1t7";
-    $database_in_use = "test";
+    $database_in_use = "p02svjwrxx0i9j6d";
 
     $mysqli = new mysqli($host, $username, $password, $database_in_use);
 
